1e0a9fd11599b2289770c9a1b79b82ecdb957a43
Casper Scholly <caspers@scholly.co.za>
2019-02-16 16:51:47 +0200

    menu

--- includes/menu.php
+++ includes/menu.php
@@ -11,7 +11,7 @@
 <div class="collapse navbar-collapse">
     <ul class="nav navbar-nav">
         <li <?php if ($page=='index.php' ) { ?>class="active"
-        <?php } ?>><a href="index.php">PAYE Calculator<span class="label label-info">2019</a></a></li>
+        <?php } ?>><a href="index.php">PAYE Calculator</a></li>
         <li <?php if ($page=='paye-information.php' ) { ?>class="active"
         <?php } ?>><a href="paye-information.php">PAYE Info</li>
         <li><a href="mailto:help@payecalculator.co.za">Suggestions?</a></li>
