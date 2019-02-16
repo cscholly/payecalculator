f4986ed05f8f31bae984b7fafe5bf4e9e5ae0514
Casper Scholly <caspers@scholly.co.za>
2019-02-16 17:24:17 +0200

    fix div

--- index.php
+++ index.php
@@ -97,6 +97,7 @@
                   </div>
                 </div>
               </div>
+            </div>
             <!-- Adsense Desktop Start -->
             <div class="panel panel-default hidden-xs hidden-sm">
               <div class="panel-body text-center">
