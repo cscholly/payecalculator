1127a96a684d9901f493f8301e1796ea0e1c74e3
Casper Scholly <caspers@scholly.co.za>
2019-02-16 17:04:22 +0200

    fix bliksemse menu

--- includes/menu.php
+++ includes/menu.php
@@ -5,15 +5,15 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
     </button>
-    <a class="navbar-brand" href="http://www.payecalculator.co.za/develop/index.php">PAYE Calculator for 2019</a>
+    <a class="navbar-brand" href="http://www.payecalculator.co.za">PAYE Calculator for 2019</a>
 </div>
 
 <div class="collapse navbar-collapse">
     <ul class="nav navbar-nav">
         <li <?php if ($page=='index.php' ) { ?>class="active"
-        <?php } ?>><a href="index.php">PAYE Calculator</a></li>
+        <?php } ?>><a href="index.php">PAYE Calculator<span class="label label-info">2019</a></a></li>
         <li <?php if ($page=='paye-information.php' ) { ?>class="active"
-        <?php } ?>><a href="paye-information.php">PAYE Info</li>
+        <?php } ?>><a href="paye-information.php">PAYE Info</a></li>
         <li><a href="mailto:help@payecalculator.co.za">Suggestions?</a></li>
     </ul>
 </div>
