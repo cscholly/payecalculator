9ac7409659d8eca3f7244d8c2fc83faaaa506182
Casper Scholly <caspers@scholly.co.za>
2019-02-16 16:47:22 +0200

    Menu fix

--- includes/menu.php
+++ includes/menu.php
@@ -5,7 +5,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
     </button>
-    <a class="navbar-brand" href="http://www.payecalculator.co.za/develop/index,php">PAYE Calculator for 2019</a>
+    <a class="navbar-brand" href="http://www.payecalculator.co.za/develop/index.php">PAYE Calculator for 2019</a>
 </div>
 
 <div class="collapse navbar-collapse">
